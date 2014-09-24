637e824f3dcc72a3c2c91a424430404c3b677041
bretzel12 <bretzel12@users.noreply.github.com>
2014-09-23 23:55:53 -0700

    Remove license - add to public domain.

--- Cable.hpp
+++ Cable.hpp
@@ -1,20 +1,3 @@
-Copyright 2014 - Transmission Line Software
-
-    This file is part of transmission-line.
-
-    transmission-line is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    transmission-line is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with transmission-line.  If not, see <http://www.gnu.org/licenses/>.
-
 #pragma once
 
 #include <list>
