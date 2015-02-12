41a17d1b0b5338beeba321f6411260843bf87f57
brett bowers <brettbowers12@users.noreply.github.com>
2015-02-11 21:36:02 -0800

    Updated Cable, CableComponent, CableLoadCase, and WeatherLoadCase to new style conventions.

---
+++ src/CableLoadCase.h
@@ -0,0 +1,40 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#ifndef TRANSMISSIONLINE_CABLELOADCASE_H_
+#define TRANSMISSIONLINE_CABLELOADCASE_H_
+
+#include <list>
+#include <string>
+
+#include "SupportLibrary.h"
+
+struct CableLoadCase {
+public:
+  /**
+   * @brief Default constructor.
+   */
+  CableLoadCase();
+
+  /**
+   * @brief Destructor.
+   */
+  ~CableLoadCase();
+
+  /**
+   * @brief Validates class data.
+   * @param is_included_warnings A flag that tightens the acceptable value
+   *        range.
+   * @param messages_error A list of detailed error messages. If this is
+   *        provided, any validation errors will be appended to the list.
+   * @return A boolean value indicating status of class data.
+   */
+  bool Validate(bool is_included_warnings = true,
+                std::list<std::string>* messages_error = nullptr) const;
+
+  // member variables
+  Vector2D  load_unit;
+  double    temperature_cable;
+};
+
+#endif  // TRANSMISSIONLINE_CABLELOADCASE_H_
---
+++ src/WeatherLoadCase.cc
@@ -0,0 +1,69 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#include "WeatherLoadCase.h"
+
+#include <cmath>
+
+#include "SupportLibrary.h"
+
+WeatherLoadCase::WeatherLoadCase()
+{
+  density_ice = -999999;
+  description = "";
+  pressure_wind = -999999;
+  temperature_cable = -999999;
+  thickness_ice = -999999;
+}
+
+WeatherLoadCase::~WeatherLoadCase()
+{}
+
+bool WeatherLoadCase::Validate(bool is_included_warnings,
+                               std::list<std::string>* messages_error) const
+{
+  bool is_valid = true;
+
+  // validate density-ice
+  if (density_ice < 0
+    || ((100 < density_ice) && (is_included_warnings == true))) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("WEATHER LOAD CASE - Invalid ice density");
+    }
+  }
+
+  // validate pressure-wind
+  if (pressure_wind < 0
+    || ((25 < pressure_wind) && (is_included_warnings == true))) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("WEATHER LOAD CASE - Invalid wind pressure");
+    }
+  }
+
+  // validate temperature-cable
+  if (temperature_cable < -50
+    || ((500 < temperature_cable) && (is_included_warnings == true))) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("WEATHER LOAD CASE - Invalid cable "
+                                "temperature");
+    }
+  }
+
+  // validate thickness-ice
+  if (thickness_ice < 0
+    || ((4 <thickness_ice) && (is_included_warnings == true))) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("WEATHER LOAD CASE - Invalid ice thickness");
+    }
+  }
+
+  return is_valid;
+}
