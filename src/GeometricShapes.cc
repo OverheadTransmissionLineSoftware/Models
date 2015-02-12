cee9c16c7db564274565e80a0e159971b3d21620
bretzel12 <bretzel12@users.noreply.github.com>
2015-02-11 22:04:31 -0800

    Updated geometric shapes file coding style

--- src/GeometricShapes.cc
+++ src/GeometricShapes.cc
@@ -1,109 +1,75 @@
 // This is free and unencumbered software released into the public domain.
 // For more information, please refer to <http://unlicense.org/>
 
+#include "GeometricShapes.h"
+
 #include <cmath>
 
-#include "GeometricShapes.hpp"
-#include "SupportLibrary.hpp"
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////// CONSTRUCTOR / DESTRUCTOR ////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
+#include "SupportLibrary.h"
 
 Cylinder::Cylinder()
 {
-    m_Diameter = -999999;
-    m_Length = -999999;
+  diameter_ = -999999;
+  length_ = -999999;
 }
 
 Cylinder::~Cylinder()
 {}
 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////// PUBLIC MEMBER FUNCTIONS /////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-/**
- * This function should be used before extracting any information from the class.
- *
- * Data errors indicate that a class data member contains an invalid value, and that the class will
- * provide unpredictable results and may generate run-time calculation errors.
- * Data warnings indicate that a class data member contains an extreme value that is typically
- * outside of the normal bounds, but will still provide a calculable result.
- */
-std::list<std::string> Cylinder::CheckData(bool includeWarnings) const
+double Cylinder::AreaCrossSection()
 {
-    std::list<std::string> checkList;
+  return (pi / 4) * pow(diameter_, 2);
+}
 
-    // diameter
-    if (m_Diameter <= 0)
-    {
-        checkList.push_back("CYLINDER - Invalid diameter");
+bool Cylinder::Validate(bool is_included_warnings,
+                        std::list<std::string>* messages_error) const
+{
+  bool is_valid = true;
+
+  // validate diameter
+  if (diameter_ <= 0
+      || ((100 < diameter_) && (is_included_warnings == true))) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("CYLINDER - Invalid diameter");
     }
-    else if (100 < m_Diameter && includeWarnings == true)
-    {
-        checkList.push_back("CYLINDER - Diameter exceeds 100 inches");
+  }
+
+  // validate length
+  if (length_ <= 0
+    || ((120 < length_) && (is_included_warnings == true))) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("CYLINDER - Invalid length");
     }
+  }
 
-    // length
-    if (m_Length <= 0)
-    {
-        checkList.push_back("CYLINDER - Invalid length");
-    }
-    else if (120 < m_Length && includeWarnings == true)
-    {
-        checkList.push_back("CYLINDER - Length exceeds 120 inches");
-    }
-
-    return checkList;
+  return is_valid;
 }
 
-double Cylinder::Get_Diameter()
+double Cylinder::Volume()
 {
-    return m_Diameter;
+  return AreaCrossSection() * length_;
 }
 
-double Cylinder::Get_CrossSectionArea()
+double Cylinder::diameter()
 {
-    return (m_Diameter * pi);
+  return diameter_;
 }
 
-double Cylinder::Get_Length()
+double Cylinder::length()
 {
-    return m_Length;
+  return length_;
 }
 
-double Cylinder::Get_SurfaceArea()
+void Cylinder::set_diameter(const double& diameter)
 {
-    return 0;
+  diameter_ = diameter;
 }
 
-double Cylinder::Get_Volume()
+void Cylinder::set_length(const double& length)
 {
-    return (pi / 4) * pow(m_Diameter, 2) * m_Length;
-}
-
-std::list<std::string> Cylinder::SaveData() const
-{
-    std::list<std::string> saveList;
-
-    saveList.push_back("[CYLINDER]");
-    saveList.push_back("diameter			= " + std::to_string(m_Diameter));
-    saveList.push_back("length				= " + std::to_string(m_Length));
-
-    return saveList;
-}
-
-void Cylinder::Set_Diameter(const double& Diameter)
-{
-    m_Diameter = Diameter;
-}
-
-void Cylinder::Set_Length(const double& Length)
-{
-    m_Length = Length;
+  length_ = length;
 }
