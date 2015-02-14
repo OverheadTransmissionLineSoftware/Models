00e928d7f0ddea4e234d7d9b86ab6ab6ad8a999d
brett bowers <brettbowers12@users.noreply.github.com>
2015-02-13 17:39:34 -0800

    Updated header and part of implementation. Still much to do.

--- src/Catenary.h
+++ src/Catenary.h
@@ -1,193 +1,295 @@
 // This is free and unencumbered software released into the public domain.
 // For more information, please refer to <http://unlicense.org/>
 
-#pragma once
+#ifndef TRANSMISSIONLINE_CATENARY_H_
+#define TRANSMISSIONLINE_CATENARY_H_
 
-#include <string>
 #include <list>
-#include <vector>
+#include <string>
 
-#include "SupportLibrary.hpp"
+#include "SupportLibrary.h"
 
 /**
  * @par CATENARY OVERVIEW
  *
- * This class models a catenary curve between two end points. The shape of the catenary curve is
- * defined by the horizontal tension and unit weight, while the endpoint spacing defines the portion
- * of the curve that is used.
+ * This class models a 2D catenary.
+ *
+ * The shape of the catenary curve is defined by the horizontal tension and unit
+ * weight, while the endpoit spacing defines the portion of the curve that is
+ * used.
  *
  * @par COORDINATE SYSTEM
  *
- * The coordinate system origin (0,0) is the catenary lowpoint.
+ * The coordinate system origin (0,0) is the catenary lowpoint, unless shifted.
+ * A shifted system uses the left endpoint as the origin.
+ *  x = horizontal
+ *  y = vertical
  *
  * @par ASSUMPTIONS
  *
  * - constant horizontal tension
  * - uniform unit weight
- *
- * @par INPUT VERIFICATION
- *
- * This class contains built-in input verification. To avoid runtime errors when using this class,
- * use the error checking functionality before extracting information.
  */
-class Catenary2D
-{
+class Catenary2D {
 public:
-    /**
-     * @brief Default constructor.
-     */
-    Catenary2D();
+  /**
+   * @brief Default constructor.
+   */
+  Catenary2D();
 
-    /**
-     * @brief Destructor.
-     */
-    ~Catenary2D();
+  /**
+   * @brief Destructor.
+   */
+  ~Catenary2D();
 
-    /**
-     * @brief Check for errors and warnings in class data.
-     * @param includeWarnings An option that includes data warnings in the return list.
-     * @return A list of strings containing class data errors/warnings.
-     */
-    std::list<std::string> CheckData(bool includeWarnings = true) const;
+  /**
+   * @brief Gets the constant (H/w), which determines the shape of the curve.
+   * @return The constant of the curve.
+   */
+  double Constant() const;
 
-    /**
-     * @brief Get the constant (H/w), which determines the shape of the curve.
-     * @return A copy of the curve constant.
-     */
-    double Get_Constant() const;
+  /**
+   * @brief Gets a coordinate point at a location on the curve.
+   * @param position_fraction A decimal ranging from 0 and 1  that describes
+   *        position on the curve by the fraction of curve length from the left
+   *        end point.
+   * @param is_shifted_origin A flag that shifts the coordinate system origin to
+   *        the left end point.
+   * @return A 2D coordinate point containing the x and y position of the
+   *         catenary curve.
+   */
+  Point2D Coordinate(const double& position_fraction,
+                     const bool& is_shifted_origin = false) const;
+  /**
+   * @brief Gets a coordinate point at a location on the straight line
+   *        connecting the end points.
+   * @param position_fraction A decimal ranging from 0 and 1  that describes
+   *        position on the curve by the fraction of curve length from the left
+   *        end point.
+   * @param is_shifted_origin A flag that shifts the coordinate system origin to
+   *        the left end point.
+   * @return A 2D coordinate point containing the x and y position of the
+   *         straight line connecting the end points.
+   */
+  Point2D CoordinateChord(const double& position_fraction,
+                          const bool& is_shifted_origin = false) const;
 
-    /**
-     * @brief Get a coordinate point at a location along the curve.
-     * @param positionAlongCurve_Fraction A number between 0 and 1 describing position on the curve.
-     * @return A coordinate point containing the x and y position of the catenary curve.
-     */
-    Point2D Get_Coordinate(const double& positionAlongCurve_Fraction) const;
-    /**
-     * @brief
-     * @return
-     */
-    Point2D Get_Coordinate_Chord(const double& positionAlongCurve_Fraction) const;
+  /**
+   * @brief Gets the curve length.
+   * @return The curve length between endpoints.
+   */
+  double Length() const;
 
-    /**
-     * @brief Get the endpoint spacing vector, starting from back-on-line and looking ahead-on-line.
-     * @return A copy of the endpoint spacing vector. x is horizontal and y is vertical.
-     */
-    Vector2D Get_EndPointSpacing() const;
+  /**
+   * @brief Gets the length of catenary slack.
+   * @return The difference in length between the curve length and straight-line
+   *         length.
+   */
+  double LengthSlack() const;
 
-    /**
-     * @brief Get the horizontal tension.
-     * @return A copy of the curve horizontal tension.
-     */
-    double Get_HorizontalTension() const;
+  /**
+   * @brief Gets the position fraction of the origin.
+   * @return The position fraction of the origin, or the lowpoint.
+   */
+  double PositionFractionOrigin() const;
 
-    /**
-     * @brief Get the curve length.
-     * @return The curve length between endpoints.
-     */
-    double Get_Length() const;
+  /**
+   * @brief Gets the position fraction of the sag point.
+   * @return The position fraction of the sag point, or where the tangent line
+   *         of the catenary is parallel with the straight line between
+   *         endpoints.
+   */
+  double PositionFractionSagPoint() const;
 
-    /**
-     * @brief Get a tangent unit vector.
-     * @param positionAlongCurve_Fraction A number between 0 and 1 describing position on the curve.
-     * @param direction The direction of the vector. Can be "BOL" for back-on-line, or "AOL" for ahead-on-line.
-     * @return A tangent unit vector.
-     */
-    double Get_Tangent_Angle(const double& positionAlongCurve_Fraction,
-                             const std::string& direction) const;
+  /**
+   * @brief Gets the sag of the catenary.
+   * @return The vertical distance bewteen the straight line and the catenary
+   *         the sag point.
+   */
+  double Sag() const;
 
-    /**
-     * @brief Get a tangent unit vector.
-     * @param positionAlongCurve_Fraction A number between 0 and 1 describing position on the curve.
-     * @param direction The direction of the vector. Can be "BOL" for back-on-line, or "AOL" for ahead-on-line.
-     * @return A tangent unit vector.
-     */
-    Vector2D Get_Tangent_Vector(const double& positionAlongCurve_Fraction, const std::string& direction) const;
+  /**
+   * @brief Gets a tangent angle from the horizontal axis to the curve tangent
+   *        line.
+   * @param position_fraction A decimal ranging from 0 and 1  that describes
+   *        position on the curve by the fraction of curve length from the left
+   *        end point.
+   * @param direction The axis direction of the tangent line.
+   * @return A tangent angle.
+   */
+  double TangentAngle(const double& position_fraction,
+                      const AxisDirectionType& direction) const;
 
-    /**
-     * @brief Get the tension.
-     * @param positionAlongCurve_Fraction    A number between 0 and 1 describing position on the curve.
-     * @return A tension magnitude.
-     */
-    double Get_Tension(const double& positionAlongCurve_Fraction) const;
+  /**
+   * @brief Gets a tangent unit vector.
+   * @param position_fraction A decimal ranging from 0 and 1  that describes
+   *        position on the curve by the fraction of curve length from the left
+   *        end point.
+   * @param direction The axis direction of the tangent line.
+   * @return A tangent unit vector.
+   */
+  Vector2D TangentVector(const double& position_fraction,
+                         const AxisDirectionType& direction) const;
 
-    /**
-     * @brief Get the tension.
-     * @param positionAlongCurve_Fraction    A number between 0 and 1 describing position on the curve.
-     * @param direction    The direction of the vector. Can be 'BOL' for back-on-line, or 'AOL' for ahead-on-line.
-     * @return A tension vector.
-     */
-    Vector2D Get_Tension(const double& positionAlongCurve_Fraction, const std::string& direction) const;
+  /**
+   * @brief Gets the tension.
+   * @param position_fraction A decimal ranging from 0 and 1  that describes
+   *        position on the curve by the fraction of curve length from the left
+   *        end point.
+   * @return The tension at the location on the catenary.
+   */
+  double Tension(const double& position_fraction) const;
 
-    /**
-     * @brief Get the tension.
-     * @param positionAlongCurve_Fraction    A number between 0 and 1 describing position on the curve.
-     * @return A tension magnitude.
-     */
-    double Get_Tension_Effective(const double& positionAlongCurve_Fraction) const;
+  /**
+   * @brief Gets the tension.
+   * @param position_fraction A decimal ranging from 0 and 1  that describes
+   *        position on the curve by the fraction of curve length from the left
+   *        end point.
+   * @param direction The axis direction of the tangent line.
+   * @return A tension vector.
+   */
+  Vector2D Tension(const double& position_fraction,
+                   const AxisDirectionType& direction) const;
 
-    /**
-     * @brief Get the unit weight.
-     * @return A copy of the unit weight.
-     */
-    double Get_UnitWeight() const;
+  /**
+   * @brief Gets the average catenary tension.
+   * @param num_points The number of points used to calculate average tension.
+   *        If zero is specified, the calculation technique will use the
+   *        Ehrenburg approximation method.
+   * @return The average tension of the catenary.
+   */
+  double TensionAverage(const int& num_points = 100) const;
 
-    /**
-     * @brief Set the endpoint spacing.
-     * @param x The horizontal separation of the endpoints, starting from back-on-line and looking ahead-on-line.
-     * @param y The vertical spacing of the endpoints, starting from back-on-line and looking ahead-on-line.
-     */
-    void Set_EndpointSpacing(const double& x, const double& y);
+  /**
+   * @brief Gets the maximum tension of the catenary.
+   * @return The maximum tension, which is at the highest end point.
+   */
+  double TensionMax() const;
 
-    /**
-     * @brief Set the horizontal tension.
-     * @param horizontalTension The horizontal tension, which is the tension at the lowpoint. The
-     *        horizontal tension is perpendicular to the unit weight.
-     */
-    void Set_Tension(const double& tension, const double& positionAlongCurve_Fraction = -1);
+  /**
+   * @brief Validates class data.
+   * @param is_included_warnings A flag that tightens the acceptable value
+   *        range.
+   * @param messages_error A list of detailed error messages. If this is
+   *        provided, any validation errors will be appended to the list.
+   * @return A boolean value indicating status of class data.
+   */
+  bool Validate(bool is_included_warnings = true,
+                std::list<std::string>* messages_error = nullptr) const;
 
-    /**
-     * @brief Set the unit weight.
-     * @param unitWeight The unit weight. The unit weight is perpendicular to the horizontal tension.
-     */
-    void Set_UnitWeight(const double& unitWeight);
+  /**
+   * @brief Sets the endpoint spacing.
+   * @param x The vector spacing between the left and right end point. The
+   *        vector starts at the left point and ends at the right point.
+   */
+  void set_spacing_endpoints(const Vector2D& spacing_endpoints);
 
-protected:
+  /**
+   * @brief Sets the horizontal tension.
+   * @param tension_horizontal The horizontal tension, which is the tension at
+   *        the lowpoint/origin. The horizontal tension is perpendicular to the
+   *        unit weight.
+   */
+  void set_tension_horizontal(const double& tension_horizontal);
 
-    /**
-     * @brief Get the x coordinate.
-     * @param curveLength_FromOrigin The curve length from the origin.
-     * @param direction The side of the origin that the coordinate is on. Can be either "BOL" or "AOL".
-     * @return The x coordinate for the specified position.
-     */
-    double Coordinate_x(const double& curveLength_FromOrigin, const std::string& direction) const;
+  /**
+   * @brief Sets the unit weight.
+   * @param weight_unit The unit weight, which is the uniform load on the
+   *        catenary. The unit weight is perpendicular to the horizontal
+   *        tension.
+   */
+  void set_weight_unit(const double& weight_unit);
 
-    /**
-     * @brief Get the y coordinate.
-     * @param coordinate_x The x coordinate for the specified position.
-     * @return The y coordinate for the specified position. Does not include H/W elevation shift (origin y coordinate = 0).
-     */
-    double Coordinate_y(const double& coordinate_x) const;
+  /**
+   * @brief Gets the endpoint spacing vector, starting from the left point and
+   *        ending at the right point.
+   * @return A copy of the endpoint spacing vector.
+   */
+  Vector2D spacing_endpoints() const;
 
-    /**
-     * @brief Get the curve length from the origin for the specified position.
-     * @param coordinate_x The x coordinate for the specified position.
-     */
-    double Get_CurveLength_FromOrigin(const double& coordinate_x) const;
+  /**
+   * @brief Gets the horizontal tension.
+   * @return A copy of the curve horizontal tension.
+   */
+  double tension_horizontal() const;
 
-    /**
-     * @brief Update the coordinate system so that the lowpoint is the origin (0, 0).
-     */
-    void Update_EndPointCoordinates() const;
+  /**
+   * @brief Gets the unit weight.
+   * @return A unit weight.
+   */
+  double weight_unit() const;
 
-    // member variables
-    double		    m_HorizontalTension;
-    double		    m_UnitWeight;
-    Vector2D	    m_EndPointSpacing;
-    mutable Point2D 	    m_EndPointBOL;
-    mutable Point2D 	    m_EndPointAOL;
+private:
 
-    mutable double  m_PositionAlongCurve_Fraction;
-    mutable bool 	bUpdateRequired;
+  /**
+   * @brief Gets the x coordinate of the catenary. This function removes the
+   *        H/w constant ordinate shift.
+   * @param length_origin_to_position The length of the curve from the origin to
+   *        the position.
+   * @param direction_origin_to_position The direction from the origin to the
+   *        position.
+   * @return The x coordinate for the specified position.
+   */
+  double CoordinateX(const double& length_origin_to_position,
+                     const AxisDirectionType& direction_origin_to_position)
+  const;
+
+  /**
+   * @brief Gets the y coordinate of the catenary. This function removes the
+   *        H/w constant ordinate shift.
+   * @param length_origin_to_position The length of the curve from the origin to
+   *        the position.
+   * @param direction_origin_to_position The direction from the origin to the
+   *        position.
+   * @return The y coordinate for the specified position.
+   */
+  double CoordinateY(const double& length_origin_to_position,
+                     const AxisDirectionType& direction_origin_to_position)
+  const;
+
+  /**
+   * @brief Gets the length from the origin for the specified position.
+   * @param coordinate The coordinate for the specified position.
+   * @return The curve length from the catenary origin to the coordinate.
+   */
+  double LengthFromOrigin(const Point2D& coordinate) const;
+
+  /**
+   * @brief Determines if class is updated.
+   * @return A boolean indicating if class is updated.
+   */
+  bool IsUpdated() const;
+
+  /**
+   * @brief Gets the fraction fo the length that relates to a specific position
+   *        on the catenary. This is referenced fromt he left end point.
+   * @param tangent_angle The tangent angle of a speicifc position on the
+   *        catenary.
+   * @return The fraction of the length for the catenary position, from the left
+   *         end point.
+   */
+  double PositionFraction(const double& tangent_angle) const;
+
+  /**
+   * @brief Updates cached member variables and modifies control variables if
+   *        update is required.
+   * @return A boolean indicating if class updates completed successfully.
+   */
+  bool Update() const;
+
+  /**
+   * @brief Updates the endpoint coordinates.
+   * @return A boolean indicating the success status of the update.
+   */
+  void UpdateEndPoints() const;
+
+  // member variables
+  mutable bool    is_updated_points_end_;
+  mutable Point2D point_end_left_;
+  mutable Point2D point_end_right_;
+  Vector2D        spacing_endpoints_;
+  double          tension_horizontal_;
+  double          weight_unit_;
 };
 
 
@@ -324,10 +426,12 @@ protected:
 //    void        Update_CatenaryFit() const;
 //
 //    // member variables
-//    Vector3D 	m_AttachmentSpacing;
-//    double 		m_CatenaryHorizontalTension;
+//    Vector3D  m_AttachmentSpacing;
+//    double    m_CatenaryHorizontalTension;
 //
-//    mutable Catenary 	m_Catenary;
+//    mutable Catenary  m_Catenary;
 //    mutable bool        bUpdateRequired_CatenaryFit;
 //};
 //
+
+#endif // TRANSMISSIONLINE_CATENARY_H_
