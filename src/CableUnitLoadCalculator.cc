f1a5ccd01b90c532c0239a2754ca4207c7da1035
brett bowers <brettbowers12@users.noreply.github.com>
2015-02-22 16:05:40 -0800

    Updated code formatting style

--- src/CableUnitLoadCalculator.cc
+++ src/CableUnitLoadCalculator.cc
@@ -1,89 +1,99 @@
-//// This is free and unencumbered software released into the public domain.
-//// For more information, please refer to <http://unlicense.org/>
-//
-//#include <cmath>
-//
-//#include "include/GeometricShapes.h"
-//#include "include/CableUnitLoadCalculator.h"
-//
-//
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////// CONSTRUCTOR / DESTRUCTOR ////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//CableUnitLoadCalculator::CableUnitLoadCalculator()
-//{}
-//
-//CableUnitLoadCalculator::~CableUnitLoadCalculator()
-//{}
-//
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////// PUBLIC MEMBER FUNCTIONS /////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-///**
-// * This function should be used before extracting any information from the class.
-// *
-// * Data errors indicate that a class data member contains an invalid value, and that the class will
-// * provide unpredictable results and may generate run-time calculation errors.
-// * Data warnings indicate that a class data member contains an extreme value that is typically
-// * outside of the normal bounds, but will still provide a calculable result.
-// */
-//std::list<std::string> CableUnitLoadCalculator::CheckData(bool includeWarnings) const
-//{
-//    std::list<std::string> checkList;
-//
-//    // check cable diameter
-//    if (m_Diameter_Cable < 0)
-//    {
-//        checkList.push_back("CABLE UNIT LOAD CALCULATOR - Invalid cable diameter");
-//    }
-//
-//    // check cable weight
-//    if (m_UnitWeight_Cable < 0)
-//    {
-//        checkList.push_back("CABLE UNIT LOAD CALCULATOR - Invalid cable unit weight");
-//    }
-//
-//    return checkList;
-//}
-//
-///**
-// * This routine solves for the vertical and transverse components of the unit loading on the cable.
-// *
-// * The vertical load on the cable is solved using the following formula:
-// * \f[ V = w + \left(dv) \f]
-// *
-// * The transverse load on the cable is solved using the following formula:
-// * \f[ T = AP \f]
-//*/
-//Vector2D CableUnitLoadCalculator::Get_UnitLoad(const LoadCase_Weather& loadCase) const
-//{
-//    Vector2D unitLoad;
-//
-//    // check diameter and weight
-//    std::list<std::string> errorList = CheckData(false);
-//    if ( 0 < errorList.size() )
-//    {
-//        return unitLoad;
-//    }
-//
-//    Cylinder bareCable;
-//    bareCable.Set_Diameter(m_Diameter_Cable / 12);
-//    bareCable.Set_Length(1);
-//
-//    Cylinder icedCable;
-//    icedCable.Set_Diameter( (m_Diameter_Cable + (2 * loadCase.m_IceThickness)) / 12 );
-//    icedCable.Set_Length(1);
-//
-//    const double iceVolume = icedCable.Get_Volume() - bareCable.Get_Volume();
-//    const double iceDensity = loadCase.m_IceDensity;
-//    const double iceWeight = iceVolume * iceDensity;
-//
-//    // transverse unit weight
-//    unitLoad.x = ( icedCable.Get_Diameter() * icedCable.Get_Length() ) * loadCase.m_WindPressure;
-//    // vertical unit weight
-//    unitLoad.y = m_UnitWeight_Cable + iceWeight;
-//
-//    return unitLoad;
-//}
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#include <cmath>
+
+#include "include/GeometricShapes.h"
+#include "include/CableUnitLoadCalculator.h"
+
+CableUnitLoadCalculator::CableUnitLoadCalculator()
+{
+  diameter_cable_ = -999999;
+  weight_unit_cable_ = -999999;
+}
+
+CableUnitLoadCalculator::~CableUnitLoadCalculator()
+{}
+
+bool CableUnitLoadCalculator::Validate(bool is_included_warnings,
+                              std::list<std::string>* messages_error) const
+{
+  bool is_valid = true;
+
+  // validate diameter-cable
+  if (diameter_cable_ <= 0) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("CABLE UNIT LOAD CALCULATOR - Invalid cable "
+                                "diameter");
+    }
+  }
+
+  // validate weight-unit-cable
+  if (weight_unit_cable_ <= 0) {
+
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("CABLE UNIT LOAD CALCULATOR - Invalid cable "
+                                "diameter");
+    }
+  }
+
+  return is_valid;
+}
+
+/**
+ * The vertical load on the cable is solved using the following formula:
+ * \f[ V = w + \left(dv) \f]
+ *
+ * The transverse load on the cable is solved using the following formula:
+ * \f[ T = AP \f]
+*/
+Vector2D CableUnitLoadCalculator::UnitCableLoad(
+                              const WeatherLoadCase& case_load_weather) const
+{
+
+  Cylinder cylinder_bare;
+  cylinder_bare.set_diameter(diameter_cable_ / 12);
+  cylinder_bare.set_length(1);
+
+  Cylinder cylinder_iced;
+  cylinder_iced.set_diameter( (diameter_cable_
+                                + (2 * case_load_weather.thickness_ice)) /12 );
+  cylinder_iced.set_length(1);
+
+  const double volume_ice = cylinder_iced.Volume() - cylinder_bare.Volume();
+  const double density_ice = case_load_weather.density_ice;
+  const double weight_ice = volume_ice * density_ice;
+
+  // transverse unit weight
+  Vector2D load_unit_cable;
+  load_unit_cable.set_x(cylinder_iced.diameter() * cylinder_iced.length()
+                          * case_load_weather.pressure_wind);
+
+  // vertical unit weight
+  load_unit_cable.set_y(weight_unit_cable_ + weight_ice);
+
+  return load_unit_cable;
+}
+
+double CableUnitLoadCalculator::diameter_cable() const
+{
+  return diameter_cable_;
+}
+
+double CableUnitLoadCalculator::weight_unit_cable() const
+{
+  return weight_unit_cable_;
+}
+
+void CableUnitLoadCalculator::set_diameter_cable(const double& diameter_cable)
+{
+  diameter_cable_ = diameter_cable;
+}
+
+void CableUnitLoadCalculator::set_weight_unit_cable(const double& weight_unit_cable)
+{
+  weight_unit_cable_ = weight_unit_cable;
+}
