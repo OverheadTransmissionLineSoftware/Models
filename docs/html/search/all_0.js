9af17a2133d30d9eae6496927fa03cf05c0621c0
bretzel12 <bretzel12@users.noreply.github.com>
2015-02-23 23:37:08 -0800

    Removed generated files. Keeping doxygen config file so they can be re-generated.

--- docs/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['angle',['Angle',['../class_vector2_d.html#accc9e6194f18c7a8dad7de81a8e23235',1,'Vector2D::Angle()'],['../class_vector3_d.html#a4742d1818b7cceeae80b9e3449c37502',1,'Vector3D::Angle()']]],
-  ['areacrosssection',['AreaCrossSection',['../class_cylinder.html#ac89e16b5833f3ffd3045b5a078a818cc',1,'Cylinder']]]
-];
