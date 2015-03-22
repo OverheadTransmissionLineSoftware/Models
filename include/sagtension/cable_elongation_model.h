acfd63b360d0a062d808e7efae6dad2dde5f4d0a
bretzel12 <bretzel12@users.noreply.github.com>
2015-03-22 00:37:40 -0700

    Initial stab at cable elongation model

---
+++ include/sagtension/cable_elongation_model.h
@@ -0,0 +1,12 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#ifndef TLSLIBRARIES_SAGTENSION_CABLEELONGATIONMODEL_H_
+#define TLSLIBRARIES_SAGTENSION_CABLEELONGATIONMODEL_H_
+
+
+class CableElongationModel {
+
+}
+
+#endif // TLSLIBRARIES_SAGTENSION_CABLEELONGATIONMODEL_H_
