3bca19e2e61973051df0aef7e178080f28a3ffe2
bretzel12 <bretzel12@users.noreply.github.com>
2015-03-24 13:48:01 -0700

    Added test subfolder to force linking for static libraries when testing build

---
+++ test/main.cc
@@ -0,0 +1,23 @@
+
+#include "base/point.h"
+#include "sagtension/cable_loader.h"
+#include "transmissionline/catenary.h"
+
+// When static libraries are built (TLS-Libraries) they are not linked.
+// This file contains basic declarations and test routines so that when
+// the libraries are built, they are compiled AND linked. This helps remove
+// some errors that would be present when linked against an executable.
+
+int main() {
+
+  // base library
+  Point2d point;
+
+  // transmission line library
+  Catenary3d catenary;
+
+  // sagtension library
+  CatenaryCableLoader loader;
+
+  return 0;
+}
