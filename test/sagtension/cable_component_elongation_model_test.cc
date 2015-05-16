dd78f1062c36323f73dd8616f9e487409dd3e74c
brett bowers <brettbowers12@users.noreply.github.com>
2015-05-16 12:48:52 -0700

    Added missing test function. In fixture, updated variable to coding convention naming style.

--- test/sagtension/cable_component_elongation_model_test.cc
+++ test/sagtension/cable_component_elongation_model_test.cc
@@ -3,9 +3,6 @@
 
 #include "sagtension/cable_component_elongation_model.h"
 
-#include <cmath>
-#include <vector>
-
 #include "base/convert_units.h"
 #include "gtest/gtest.h"
 
@@ -14,30 +11,30 @@ class CableComponentElongationModelTest : public ::testing::Test {
   CableComponentElongationModelTest() {
 
     // builds dependency object - component
-    const double area_physical = 0.7264;
+    const double kAreaPhysical = 0.7264;
 
     std::vector<double> coefficients_creep;
-    coefficients_creep.push_back(-544.8 * area_physical);
-    coefficients_creep.push_back(21426.8 * area_physical);
-    coefficients_creep.push_back(-18842.2 * area_physical);
-    coefficients_creep.push_back(5495 * area_physical);
-    coefficients_creep.push_back(0 * area_physical);
+    coefficients_creep.push_back(-544.8 * kAreaPhysical);
+    coefficients_creep.push_back(21426.8 * kAreaPhysical);
+    coefficients_creep.push_back(-18842.2 * kAreaPhysical);
+    coefficients_creep.push_back(5495 * kAreaPhysical);
+    coefficients_creep.push_back(0 * kAreaPhysical);
 
     std::vector<double> coefficients_loadstrain;
-    coefficients_loadstrain.push_back(-1213 * area_physical);
-    coefficients_loadstrain.push_back(44308.1 * area_physical);
-    coefficients_loadstrain.push_back(-14004.4 * area_physical);
-    coefficients_loadstrain.push_back(-37618 * area_physical);
-    coefficients_loadstrain.push_back(30676 * area_physical);
+    coefficients_loadstrain.push_back(-1213 * kAreaPhysical);
+    coefficients_loadstrain.push_back(44308.1 * kAreaPhysical);
+    coefficients_loadstrain.push_back(-14004.4 * kAreaPhysical);
+    coefficients_loadstrain.push_back(-37618 * kAreaPhysical);
+    coefficients_loadstrain.push_back(30676 * kAreaPhysical);
 
     CableComponent shell;
     shell.coefficient_expansion_linear_thermal = 0.0000128;
     shell.coefficients_polynomial_creep = coefficients_creep;
     shell.coefficients_polynomial_loadstrain = coefficients_loadstrain;
-    shell.load_limit_polynomial_creep = 7535 * area_physical;
-    shell.load_limit_polynomial_loadstrain = 20252 * area_physical;
-    shell.modulus_compression_elastic_area = 1500 * area_physical * 100;
-    shell.modulus_tension_elastic_area = 64000 * area_physical * 100;
+    shell.load_limit_polynomial_creep = 7535 * kAreaPhysical;
+    shell.load_limit_polynomial_loadstrain = 20252 * kAreaPhysical;
+    shell.modulus_compression_elastic_area = 1500 * kAreaPhysical * 100;
+    shell.modulus_tension_elastic_area = 64000 * kAreaPhysical * 100;
 
     // builds fixture object
     c_.set_component_cable(shell);
@@ -80,6 +77,24 @@ TEST_F(CableComponentElongationModelTest, PointPolynomialStart) {
   Point2d p = c_.PointPolynomialStart();
   EXPECT_EQ(0.0020, supportfunctions::Round(p.x, 4));
   EXPECT_EQ(5000, supportfunctions::Round(p.y, 1));
+}
+
+TEST_F(CableComponentElongationModelTest, PointsRegions) {
+
+  std::vector<Point2d> points = c_.PointsRegions();
+  Point2d p;
+
+  p = points.at(0);
+  EXPECT_EQ(0.000938, supportfunctions::Round(p.x, 6));
+  EXPECT_EQ(0, supportfunctions::Round(p.y, 1));
+
+  p = points.at(1);
+  EXPECT_EQ(0.0020, supportfunctions::Round(p.x, 4));
+  EXPECT_EQ(5000, supportfunctions::Round(p.y, 1));
+
+  p = points.at(2);
+  EXPECT_EQ(0.0091, supportfunctions::Round(p.x, 4));
+  EXPECT_EQ(14711.1, supportfunctions::Round(p.y, 1));
 }
 
 TEST_F(CableComponentElongationModelTest, PointUnloaded) {
