01458164f0ec212f4f932069bb74125ebba53bab
bretzel12 <bretzel12@users.noreply.github.com>
2015-05-28 21:00:21 -0700

    Added CatenaryCableUnloader testing.

---
+++ test/sagtension/catenary_cable_reloader_test.cc
@@ -0,0 +1,7 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#include "sagtension/catenary_cable_reloader.h"
+
+#include "base/convert_units.h"
+#include "gtest/gtest.h"
