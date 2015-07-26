8efb015df5d5bc09bdd20cee9ffeb6010789303f
bretzel12 <bretzel12@users.noreply.github.com>
2015-07-25 18:17:30 -0700

    Added the LineCable class.

---
+++ src/transmissionline/line_cable.cc
@@ -0,0 +1,54 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org/>
+
+#include "transmissionline/line_cable.h"
+
+LineCable::LineCable() {
+}
+
+LineCable::~LineCable() {
+}
+
+bool LineCable::Validate(const bool& is_included_warnings,
+                         std::list<std::string>* messages_error) const {
+
+  bool is_valid = true;
+
+  // validates cable
+  if (cable.Validate(is_included_warnings, messages_error) == false) {
+    is_valid = false;
+  }
+
+  // validates constraint
+  if (constraint.Validate(is_included_warnings, messages_error) == false) {
+    is_valid = false;
+  }
+
+  // validates spacing-attachments-rulingspan
+  if (spacing_attachments_ruling_span.x() <= 0) {
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("LINE CABLE - Invalid horizontal ruling span "
+                                 "attachment spacing");
+    }
+  }
+
+  if (spacing_attachments_ruling_span.y() != 0) {
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("LINE CABLE - Invalid ruling span attachment"
+                                "spacing");
+    }
+  }
+
+  if (2000 < abs(spacing_attachments_ruling_span.z())) {
+    is_valid = false;
+    if (messages_error != nullptr) {
+      messages_error->push_back("LINE CABLE - Invalid vertical ruling span"
+                                "attachment spacing");
+    }
+  }
+
+  // returns validation status
+  return is_valid;
+}
