3174f54378e1fed93464a76e20e805789b63ba88
bretzel12 <bretzel12@users.noreply.github.com>
2015-10-13 21:42:19 -0700

    Reconfigured LineCableTest to build object with factory, rather than class constructor.

--- test/transmissionline/line_cable_test.cc
+++ test/transmissionline/line_cable_test.cc
@@ -10,26 +10,7 @@
 class LineCableTest : public ::testing::Test {
  protected:
   LineCableTest() {
-    // gets cable
-    Cable* cable = factory::BuildCable();
-
-    // builds dependency object - cable constraint
-    WeatherLoadCase* case_weather = new WeatherLoadCase();
-    case_weather->description = "0-0-60";
-    case_weather->thickness_ice = 0;
-    case_weather->density_ice = 0;
-    case_weather->pressure_wind = 0;
-    case_weather->temperature_cable = 60;
-
-    CableConstraint constraint;
-    constraint.case_weather = case_weather;
-    constraint.condition = CableConditionType::kInitial;
-    constraint.limit = 6000;
-    constraint.type_limit = CableConstraint::LimitType::kHorizontalTension;
-
-    l_.cable = cable;
-    l_.constraint = constraint;
-    l_.spacing_attachments_ruling_span = Vector3d(1200, 0, 0);
+    l_ = *factory::BuildLineCable();
   }
 
   LineCable l_;
